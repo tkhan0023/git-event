bd87235862ecf7b50b7f319793a2569675f0e680
tkhan0023 <kardosepeter@gmail.com>
2024-02-03 19:51:39 +0530

    new changes

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
-//my tests
+//my tests made
 
 describe('MainContent', () => {
   it('should render a button', () => {
