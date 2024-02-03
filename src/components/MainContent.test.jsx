55ad0f79dd746ef0d77ead290cd0e41ce175858a
tkhan0023 <kardosepeter@gmail.com>
2024-02-03 20:02:56 +0530

    added comments [skip ci]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -4,7 +4,7 @@ import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
 //my tests made new changes 
-
+// add yet another commit
 describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
